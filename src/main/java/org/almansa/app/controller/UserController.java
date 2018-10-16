c2b087bc413bb07aef30f895aab00adc509c2598
skennel2 <skennel2@gmail.com>
2018-10-16 23:44:53 +0900

    bug fix

--- src/main/java/org/almansa/app/controller/UserController.java
+++ src/main/java/org/almansa/app/controller/UserController.java
@@ -2,35 +2,37 @@ package org.almansa.app.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.almansa.app.domain.User;
 import org.almansa.app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "/user")
 public class UserController {
 
 	private UserService userService;
-
+	
 	@Autowired
 	public UserController(UserService userSerivce) {
 		super();
 		this.userService = userSerivce;
 	}
 	
-	@PutMapping(path="/join")
-	public ResponseEntity<Void> joinUser(@ModelAttribute User newUser){
+	@PostMapping(path="/join")
+	public ResponseEntity<Void> joinUser(@RequestBody User newUser){
 		try{
 			userService.addUser(newUser);		
 			return new ResponseEntity<>(HttpStatus.OK);
